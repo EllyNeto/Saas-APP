5864772555d4ce26e24925ded5bdc4c568a6c1c6
EllyNeto <ellyneto12@gmail.com>
2026-06-22 04:50:12 -0700

    Add navbar que funciona e troca a cor do item

---
+++ teste/components/NavItems.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React from 'react'
+import Link from "next/link";
+import {cn} from "@/lib/utils"
+import { usePathname } from 'next/navigation';
+
+const navItems = [
+    {label:'Home', href: '/'},
+    {label: 'Companios', href: '/companions'},
+    {label: 'My Journey', href: '/dashjourney'},
+]
+
+
+
+const NavItems = () => {
+    const pathname = usePathname();
+  return (
+    <nav className="flex items-center gap-4">
+        {navItems.map(({label, href}) => (
+            <Link 
+                href={href} 
+                key={label} 
+                className={cn(pathname === href && 'text-primary font-semibold')}>
+                {label}
+            </Link>
+        ))}
+    </nav>
+  )
+}
+
+// const NavItems = () => {
+//     const pathname = usePathname();    
+//     return (
+//         <nav className="flex items-center gap-4">
+//         {navItems.map(({ label, href }) => (
+//             <Link
+//                 key={label}
+//                 href={href}
+//                 className={`${
+//                     pathname === href
+//                     ? 'text-primary font-semibold'
+//                     : 'text-gray-500'
+//                 }`}
+//                 >
+//                 {label}
+//             </Link>
+//         ))}
+//         </nav>
+//     );
+// };
+
+export default NavItems
--- teste/components/navbar.tsx
+++ teste/components/navbar.tsx
@@ -1,20 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "./NavItems";
+
 const Navbar = () => {
   return (
     <nav className="navbar"> 
-     <link href="/">
+     <Link href="/">
         <div className="flex items-center grap-2.5 cursor-pointer">
-            <Image>
-                src="/logo.png"
+            <Image
+                src="./images/logo.svg"
                 alt="Logo"
                 width={46}
                 height={44}
-            </Image>
+            />
         </div>
-     </link>
-     <div className="flex items-center grap-8">
-        <p>Home</p>
-        <p>Companions</p>
-        <p>My journey</p>
+     </Link>
+     <div className="flex items-center gap-8">
+       <NavItems/>
         <p>Sign In</p>
      </div>
        </nav>
